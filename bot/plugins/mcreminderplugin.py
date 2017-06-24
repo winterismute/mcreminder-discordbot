423e1e28547e863ab4f63ebbadc6845ef4c8dc3e
winterismute <rudibonfiglioli@gmail.com>
2017-06-24 15:21:19 +0000

    Main code

---
+++ bot/plugins/mcreminderplugin.py
@@ -0,0 +1,49 @@
+from disco.bot import Bot, Plugin
+from triggeritem import TriggerItem, TriggerItemReminder
+import re
+import json
+import sys
+
+def toTriggerItemReminder(dct):
+	return TriggerItemReminder(dct['content'], dct.get('embed', None), dct.get('attachments', []))
+
+def toTriggerItem(dct):
+	return TriggerItem(dct['type'], dct['tokens'], (dct['reminder']), dct.get('replacementTokens',[]), dct.get('cooldownTime', 0))
+
+def newjsondecode(data):
+	if 'triggers' in data:
+		return data['triggers']
+	if all(k in data for k in ('type', 'tokens', 'reminder')):
+		return toTriggerItem(data)
+	if 'content' in data:
+		return toTriggerItemReminder(data)
+	raise ValueError('Error: can not parse data: ' + str(data))
+
+class McReminderPlugin(Plugin):
+
+	def load(self, ctx):
+		try:
+			json_data = open('botconfig.json')
+		except IOError as e:
+			self.log.error('Error while opening config file: ' + str(e))
+			self.log.error('Exiting...')
+			sys.exit()
+		else:
+			with json_data:
+				try:
+					#self.triggers = json.load(json_data, object_hook = newjsondecode, encoding="cp1252")
+					self.triggers = json.load(json_data, object_hook = newjsondecode)
+					self.log.info(self.triggers)
+				except ValueError as e:
+					self.log.error('Can not process struture of botconfig.json: ' + str(e))
+					sys.exit()
+
+	@Plugin.listen('MessageCreate')
+	def on_message_create(self, event):
+		# never trigger on bot's own messages
+		if (event.author.id == self.state.me.id):
+			return
+		for trigger in self.triggers:
+			craftedMessage, craftedEmbed, craftedAttachments = trigger.satisfiesTrigger(event)
+			if craftedMessage:
+				event.reply(craftedMessage, attachments=craftedAttachments, embed=craftedEmbed)
---
+++ bot/triggeritem.py
@@ -0,0 +1,53 @@
+from disco.types.message import MessageEmbed
+import re
+
+class TriggerItemReminder(object):
+	def __init__(self, content, embed=None, attachments=[]):
+		self.content = content
+		self.embed = embed
+		self.attachments = attachments
+		self.attachmentsData = [open(apath, 'rb') for apath in self.attachments]
+
+class TriggerItem(object):
+	def __init__(self, itemType, tokens, reminder, replacementTokens=None, cooldownTime=0):
+		self.itemType = itemType
+		self.patterns = []
+		if itemType == 'regex':
+			self.patterns = [re.compile(t) for t in tokens]
+		self.reminder = reminder
+		self.replacementTokens = replacementTokens
+		self.cooldownTime = cooldownTime
+		self.cooldowns = {}
+
+	def isCooldownSatisfied(self, e):
+		if self.cooldownTime <= 0:
+			return True
+		if not e.channel_id in self.cooldowns:
+			self.cooldowns[e.channel_id] = e.timestamp
+			return True
+		if (e.timestamp - self.cooldowns[e.channel_id]).total_seconds() > self.cooldownTime:
+			self.cooldowns[e.channel_id] = e.timestamp
+			return True
+		return False
+
+	def craftReply(self, event, satisfiedPatternIndex):
+		e = None
+		# here, we check for None since empty string means "suppress embeds"
+		if not self.reminder.embed is None:
+			e = MessageEmbed()
+			e.set_image(url=self.reminder.embed)
+		atts = []
+		if self.reminder.attachments:
+			atts = [(self.reminder.attachments[i], self.reminder.attachmentsData[i]) for i in range(len(self.reminder.attachments))]
+		m = self.reminder.content
+		m = m.replace(u'$AUTHOR', u'<@' + str(event.author.id) + '>')
+		for index, t in enumerate(self.replacementTokens[satisfiedPatternIndex]):
+			m = m.replace("$" + str(index+1), t)
+		return m, e, atts
+
+	def satisfiesTrigger(self, event):
+		text = event.content.lower()
+		for index, p in enumerate(self.patterns):
+			if p.search(text) and self.isCooldownSatisfied(event):
+				return self.craftReply(event, index)
+		return None
